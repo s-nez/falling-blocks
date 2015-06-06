c572276f3d20c568da3cb105bf6425ea34474a45
Szymon Nieznański <s.nez@member.fsf.org>
2015-06-06 16:13:19 +0200

    Resolve block collisions

--- game.py
+++ game.py
@@ -1,24 +1,9 @@
 #!/usr/bin/python
 from time import sleep
 import curses
-from block import Block
 from time import time
 from random import randint
-
-SHAPES = [
-        [ [1, 1, 1], [0, 0, 1] ],
-        [ [0, 1], [1, 1], [0, 1] ],
-        [ [1, 1], [1, 1] ],
-        [ [0, 0, 1], [1, 1, 1] ],
-        [ [1, 1, 1, 1] ],
-        [ [1, 1, 0], [0, 1, 1]]
-        ]
-
-def spawn_block():
-    shape_index = randint(0, len(SHAPES) - 1)
-    block = Block(win, shape_index + 1, [1, 10], SHAPES[shape_index])
-    block.show()
-    return block
+from board import Board
 
 STEP_TIME = 0.5
 
@@ -38,38 +23,32 @@ curses.init_pair(4, curses.COLOR_CYAN, curses.COLOR_CYAN)
 curses.init_pair(5, curses.COLOR_YELLOW, curses.COLOR_YELLOW)
 curses.init_pair(6, curses.COLOR_MAGENTA, curses.COLOR_MAGENTA)
 
-BEGIN_X, BEGIN_Y = 0, 0
-HEIGHT, WIDTH = 20, 20
-win = curses.newwin(HEIGHT, WIDTH, BEGIN_Y, BEGIN_X)
-win.box()
-stdscr.refresh()
-win.refresh()
+try:
+    game_board = Board(stdscr, 0, 0, 20, 20)
 
-quit = False
-last_game_step = time()
-block = spawn_block()
-while not quit:
-    current_time = time()
-    if current_time - last_game_step > STEP_TIME:
-        block.move_down()
-        last_game_step = current_time
+    quit = False
+    last_game_step = time()
+    while not quit:
+        current_time = time()
+        if current_time - last_game_step > STEP_TIME:
+            game_board.advance_block()
+            last_game_step = current_time
 
-    c = stdscr.getch()
-    if c == ord('q'):
-        quit = True
-    elif c == ord('a'):
-        block.move_left()
-    elif c == ord('d'):
-        block.move_right()
-    elif c == ord('s'):
-        while not block.landed():
-            block.move_down()
+        c = stdscr.getch()
+        if c == ord('q'):
+            quit = True
+        elif c == ord('a'):
+            game_board.lshift_block()
+        elif c == ord('d'):
+            game_board.rshift_block()
+        elif c == ord('s'):
+            game_board.land_block()
 
-    if block.landed():
-        block = spawn_block()
+        game_board.draw()
 
-# Disable the curses-friendly terminal settings and close the window
-curses.nocbreak()
-stdscr.keypad(0)
-curses.echo()
-curses.endwin()
+finally:
+    # Disable the curses-friendly terminal settings and close the window
+    curses.nocbreak()
+    stdscr.keypad(0)
+    curses.echo()
+    curses.endwin()
